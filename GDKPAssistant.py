7102bd3e8a451e5423ef493ac52876498a26e257
integgroll <integgroll@gmail.com>
2020-09-02 17:18:44 -0500

    Converting environment to full docker-compose deployment

--- GDKPAssistant.py
+++ GDKPAssistant.py
@@ -1,13 +1,14 @@
 import os
-import loguru
 import selenium
 
+from loguru import logger
 from selenium import webdriver
-from selenium.webdriver.common import keys
-from selenium.webdriver.chrome import options
+import selenium.webdriver.chrome.service
+import selenium.webdriver.chrome.options
+
+
 
 #import pandas as pd
-from bs4 import BeautifulSoup
 
 
 
@@ -16,11 +17,17 @@ class CharacterRater:
         """
 
         """
-        selenium.webdriver.common.keys.Keys()
-        chrome_options_handle = selenium.webdriver.chrome.options.Options()
-        chrome_options_handle.add_argument("--headless")
-        chrome_options_handle.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-        self.request_driver = selenium.webdriver.Chrome(executable_path=os.path.abspath('/usr/local/bin/chromedriver'),chrome_options=chrome_options_handle)
+
+        # Yes we apparently need to run this without sandboxing OR a head to the connection for this to function properly
+        # This means that we need to also make sure that the docker container isn't using the root user when we deploy
+        chrome_options = selenium.webdriver.chrome.options.Options()
+        chrome_options.binary_location = '/usr/bin/chromium'
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        self.chrome_driver = selenium.webdriver.Chrome(options=chrome_options,executable_path='/usr/bin/chromedriver')
+
+        #self.chrome_service = selenium.webdriver.chrome.service.Service('/usr/bin/chromedriver')
+        #self.chrome_service.start()
 
     def get_character_ratings(self, character_name):
         """
@@ -29,10 +36,15 @@ class CharacterRater:
         :param character_name:
         :return:
         """
-
-        self.request_driver.get(f"https://classic.warcraftlogs.com/character/us/atiesh/{character_name}#zone=1002&partition=2")
-        bwl_perf_avg = self.request_driver.find_element_by_css_selector(".best-perf-avg>b").text
-        aq_tab = self.request_driver.find_element_by_id("partitions-tab-3")
+        self.chrome_driver.get(f"https://classic.warcraftlogs.com/character/us/atiesh/{character_name}#zone=1002&partition=2")
+        bwl_perf_avg = self.chrome_driver.find_element_by_css_selector(".best-perf-avg>b").text
+        aq_tab = self.chrome_driver.find_element_by_id("partitions-tab-3")
         aq_tab.click()
-        aq_perf_avg = self.request_driver.find_element_by_css_selector(".best-perf-avg>b").text
+        aq_perf_avg = self.chrome_driver.find_element_by_css_selector(".best-perf-avg>b").text
         return (bwl_perf_avg,aq_perf_avg)
+
+
+if __name__=="__main__":
+    rater_handle = CharacterRater()
+    logger.info(rater_handle.get_character_ratings("introgueroll"))
+    logger.info(rater_handle.get_character_ratings("spicy"))
